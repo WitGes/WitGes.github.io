a15e709494a9d66acdc265037dec4717769d5d4b
WitGes <witek_gesse@poczta.onet.pl>
2016-05-31 23:42:39 +0200

    modification files

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
-var tab=[21, 23, 111, 43, 55, 11, 7, 6, 3, 87, 34, 65, 98];
-
-var minMax=function(){
-	var min=[], max=[];
-	var k=0, max, maxTmp, min, minTmp, elementsTab, tmp;
-	var evenEl= tab.length%2==0 ? true : false;
-	evenEl ? elementsTab=tab.length : elementsTab=tab.length-1;
-	evenEl ? tmp=NULL : tmp=tab[tab.length-1];
-	for (var i=0;i<elementsTab;i+=2){
-		if (tab[i]>tab[i+1]){
-			max[k]=tab[i];
-			min[k]=tab[i+1];
-			}
-		else{
-			min[k]=tab[i];
-			max[k]=tab[i+1]
-			};
-		k++;
-	};
-	maxTmp=max[0];
-	for (var i=1;i<max.length;i++){
-		if (max[i]>maxTmp) maxTmp=max[i];
-	}
-	if (!evenEl && maxTmp<tmp) maxTmp=tmp;
-	return maxTmp;
-}
-
-
 $(document).ready(function(){
-	var phrase='[';
-	$(tab).each(function(index){
-		index<tab.length-1 ? phrase+=tab[index]+", " : phrase+=tab[index]+']';
-	});
-	$('.resolve').html(phrase+' max='+minMax());
+	start();
+});
+
+$('button').on('click', function(){
+	if (parseInt($('#newElementTab').val())) {
+		tab.push(parseInt($('#newElementTab').val()));
+		start();
+	}
+	else{
+		$('#newElementTab').attr('placeholder','Musisz podać liczbę');
+	};
+});
+
+$('ul').on('dblclick','.button',function(event){
+		tab.splice($(this).attr('value'),1);
+		start();
+});
+
+$('ul').on('click','.buttonNext',function(){
+	var tmp = tab[0];
+	tab.shift(tab.push(tmp));
+	start();
+});
+
+$('ul').on('click','.buttonPrevious',function(){
+	var tmp = tab[tab.length-1];
+	tab.unshift(tmp);
+	tab.splice(tab.length-1,1)
+	start();
 });
--- js/view.js
+++ js/view.js
@@ -1,24 +1,23 @@
-
-$(document).ready(function(){
-
-	var countTab=tab.length;
-	var str='<li class="disabled">\
-				<a href="#" aria-label="Previous">\
+function start(){
+	
+	var countTab= tab.length<10 ? tab.length : 10;
+	var str='<li class="enabled">\
+				<a class="buttonPrevious" aria-label="Previous" style="cursor:pointer">\
 					<span aria-hidden="true">&laquo;</span>\
 				</a>\
 			</li>';
 	
 	for (var i=0;i<countTab;i++){
-		str+='<li><a href="#">'+tab[i]+'</a></li>'
+		str+='<li><a class="button" style="cursor:pointer" value="'+i+'">'+tab[i]+'</a></li>'
 	};
-	str+='<li class="disabled">\
-			<a href="#" aria-label="Next">\
+	str+='<li class="enabled">\
+			<a class="buttonNext" aria-label="Next" style="cursor:pointer" >\
 				<span aria-hidden="true">&raquo;</span>\
 			</a>\
 		</li>';
 	
 	$('ul').html(str);
 	
-});
-//exports.tab=tab;
-//exports.view=view;
+	maxMinFromTab();
+	
+};
